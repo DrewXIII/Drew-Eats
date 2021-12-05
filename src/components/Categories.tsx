4ae4d82c7d831dc1277599efeb2edb40779c54dc
Andrés Lara Mesa <47040191+DrewXIII@users.noreply.github.com>
2021-12-05 14:57:09 +0100

    feat(caterogies): added catergories and category components

---
+++ src/components/Categories.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {Category} from './Category';
+
+const items = [
+  {
+    image: require('../../assets/images/shopping-bag.png'),
+    text: 'Pick-up',
+  },
+  {
+    image: require('../../assets/images/soft-drink.png'),
+    text: 'Soft Drinks',
+  },
+  {
+    image: require('../../assets/images/bread.png'),
+    text: 'Bakery Items',
+  },
+  {
+    image: require('../../assets/images/fast-food.png'),
+    text: 'Fast Foods',
+  },
+  {
+    image: require('../../assets/images/deals.png'),
+    text: 'Deals',
+  },
+  {
+    image: require('../../assets/images/coffee.png'),
+    text: 'Coffee & Tea',
+  },
+  {
+    image: require('../../assets/images/desserts.png'),
+    text: 'Desserts',
+  },
+];
+
+export const Categories = () => {
+  return (
+    <View style={styles.container}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {items.map(({image, text}, index) => (
+          <Category key={index} image={image} text={text} />
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 5,
+    backgroundColor: 'white',
+    paddingVertical: 10,
+    paddingLeft: 20,
+  },
+});
