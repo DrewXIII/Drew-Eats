efa1342f378d75f988c033916e48158c4ce5ae70
Andrés Lara Mesa <47040191+DrewXIII@users.noreply.github.com>
2021-12-06 18:58:14 +0100

    feat(home screen): added search bar funcionality and restaurant items

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,15 +1,64 @@
-import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, ScrollView, StyleSheet, View} from 'react-native';
 import {Categories} from '../components/Categories';
 import {HeaderTabs} from '../components/HeaderTabs';
+import {RestaurantItem} from '../components/RestaurantItem';
 import {SearchBar} from '../components/SearchBar';
+import {FoodService} from '../utils/enums/FoodService.enum';
+import {Restaurant} from '../utils/interfaces/Restaurant.interface';
+import {getRestaurantsByLocation} from '../utils/services/restaurants/getRestaurantsByLocation';
 
 export const HomeScreen = () => {
+  const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+  const [place, setPlace] = useState('San Diego'); // Google Place API KEY has been removed, so this value is hardcoded to see one place.
+  const [activeTab, setActiveTab] = useState<
+    FoodService.Delivery | FoodService.Pickup
+  >(FoodService.Delivery);
+
+  useEffect(() => {
+    getRestaurantsByLocation(place).then(
+      (data: {
+        businesses: Restaurant[];
+        region: {center: {longitude: number; latitude: number}};
+        total: number;
+      }) => {
+        const restaurants = data.businesses.filter((restaurant: Restaurant) =>
+          restaurant.transactions.includes(activeTab.toLowerCase()),
+        );
+
+        setRestaurants(restaurants);
+      },
+    );
+  }, [place, activeTab]);
+
   return (
-    <SafeAreaView>
-      <HeaderTabs />
-      <SearchBar />
+    <SafeAreaView style={styles.screen}>
+      <View style={styles.container}>
+        <HeaderTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+        <SearchBar setPlace={setPlace} />
+      </View>
       <Categories />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {restaurants.map(({id, image_url, name, rating}) => (
+          <RestaurantItem
+            key={id}
+            image_url={image_url}
+            name={name}
+            rating={rating}
+          />
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 15,
+  },
+  screen: {
+    backgroundColor: '#eee',
+    flex: 1,
+  },
+});
