fbf15a3c65abf0849e430e73f2e37e9b7fbfb095
Andrés Lara Mesa <47040191+DrewXIII@users.noreply.github.com>
2021-12-06 18:56:54 +0100

    refactor: removed unused element

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import {Category} from './Category';
 
 const items = [
